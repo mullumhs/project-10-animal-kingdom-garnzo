dd652329b856e00ed31b57d2f69f7d3d8cc7cb5b
Garnzo <92022699+garnzo@users.noreply.github.com>
2024-05-21 11:50:03 +1000

    so i understand this a little bit more.
    its a lil bit confusing with the inheritance a bit

--- lab1.py
+++ lab1.py
@@ -11,21 +11,43 @@
 # Create a class named Animal that represents a generic animal in an animal kingdom.
 # This class should have an initialiser with at least three attributes. E.g. name, age, and habitat.
 # Add at least two methods for common animal behaviors. E.g. eat and sleep.
+class Animal:
 
+    def __init__(self, name, type, habitat):
+        self.name = name
+        self.type = type
+        self.habitat = habitat
 
+    def hunting(self):
+        print(f"{self.name} is hunting") 
 
-
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+ 
+    def __str__(self):
+        return f"Animal: {self.name}, Type: {self.type}, Habitat: {self.habitat}"
 
 # Create at least two derived classes from the Animal class. E.g. Bird and Fish.
 # Give each of the derived classes at least one specific behavior. E.g. fly and swim.
+class Lion(Animal):
 
+    def __init__(self, name, type, habitat, mane_length):
+        super().__init__(self, name, type, habitat)
+        self.mane_length = mane_length
 
+    def growl(self):
+        print(f"{self.name} growls")
 
+    def scratch(self):
+        print(f"{self.name} scratches")
+
+    def __str__(self):
+        return f"Animal: {self.name}, Type: {self.type}, Habitat: {self.habitat}"
 
 
 # Create at least two instances of the Animal derived classes with different data.
 
-
+lion = Lion("bob", "mammal", "africa", "18m")
 
 
 
